353335fe2e710fe9d4d78efac5c7fd942513f8a3
oquie <stellanwachukwu@gmail.com>
2022-11-28 14:11:49 -0500

    dev env deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo "deployment in progress"
-mkdir deploy
-touch deploy/app.java
-echo "welcome to Tesla" >> deploy/app.java
-chmod 777 deploy/app.java
-sudo chown obi deploy/app.java
-scp
+#echo "deployment in progress"
+#mkdir deploy
+#touch deploy/app.java
+#echo "welcome to Tesla" >> deploy/app.java
+#chmod 777 deploy/app.java
+#sudo chown obi deploy/app.java
+#scp
+
+deploy java applications
+deploy python applications
+deploy in the dev env
